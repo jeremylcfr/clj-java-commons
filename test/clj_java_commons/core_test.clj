95b98032b82117c44bcd42a57bba0af5d67c2b7b
JeremyLC <jeremylecorguille@live.fr>
2022-08-24 21:43:49 +0200

    fix fixme-i-fail

--- test/clj_java_commons/core_test.clj
+++ test/clj_java_commons/core_test.clj
@@ -1,7 +1,3 @@
 (ns clj-java-commons.core-test
   (:require [clojure.test :refer :all]
             [clj-java-commons.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
