fa011c30f5c690cea6cb2557db2e6b645309526f
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:29:20 +0200

    start 1.0.0 and correct deploy artifact

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject jeremylcfr/clj-java-commons "0.1.0"
+(defproject jeremylcfr/clj-java-commons "1.0.0-SNAPSHOT"
   :description "Various Java interop and coercion shared material"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]] 
-  :repositories [["github" {:url "https://maven.pkg.github.com/jeremylcfr/clj-java-commons" 
-                            :username "jeremylcfr" 
-                            :password :env/GITHUB_TOKEN 
-                            :sign-releases false}]]
+  :repositories [["github-publish" {:url "https://maven.pkg.github.com/jeremylcfr/clj-java-commons" 
+                                    :username "jeremylcfr" 
+                                    :password :env/GITHUB_TOKEN 
+                                    :sign-releases false}]]
   :profiles {:dev {:source-paths ["repl"]
                    :dependencies [[org.clojure/tools.namespace "1.3.0"]]}}
   :repl-options {:init-ns repl})
