2f216788888710b1ef15ba406bc9b8985eddca58
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:20:11 +0200

    set repo

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]] 
+  :repositories [["github" {:url "https://maven.pkg.github.com/jeremylcfr/clj-java-commons" 
+                            :username "jeremylcfr" 
+                            :password :env/GITHUB_TOKEN 
+                            :sign-releases false}]]
   :profiles {:dev {:source-paths ["repl"]
                    :dependencies [[org.clojure/tools.namespace "1.3.0"]]}}
   :repl-options {:init-ns repl})
