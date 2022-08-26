4349a4c5d562b4eda2f1292098d7d95fe4503701
JeremyLC <jeremylecorguille@live.fr>
2022-08-26 23:13:59 +0200

    0.1.0 : initial release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jeremylcfr/clj-java-commons "0.1.0-SNAPSHOT"
+(defproject jeremylcfr/clj-java-commons "0.1.0"
   :description "Various Java interop and coercion shared material"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
